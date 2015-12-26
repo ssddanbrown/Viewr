846c11c8df1e199b28f441c2f9142a5539f22f19
Dan Brown <ssddanbrown@googlemail.com>
2015-12-26 11:26:29 +0000

    Removed jQuery dependancy

--- scripts/script.js
+++ scripts/script.js
@@ -1,62 +1,73 @@
 (function() {
     "use strict";
 
-    function isScrolledIntoView(elem) {
-        var $elem = $(elem);
-        var docViewTop = $(window).scrollTop();
-        var docViewBottom = docViewTop + $(window).height();
+    function isScrolledIntoView(elemId) {
+        var elem = document.getElementById(elemId);
+        var docViewBottom = window.innerHeight;
+        var elemBoundingRect = elem.getBoundingClientRect();
 
-        var elemTop = $elem.offset().top;
-        var elemBottom = elemTop + $elem.height();
-
-        return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+        return ((elemBoundingRect.bottom <= window.innerHeight) && (elemBoundingRect.top >= 0));
     }
 
-    var imgurService = {
+    function createImgurUrl(subreddit, pageCount, sort, time) {
+        var urlString = 'https://api.imgur.com/3/gallery/r/' + subreddit;
+        urlString += (sort === 'top') ? '/top/' + time : '/time';
+        return urlString + '/' + pageCount + '.json';
+    }
 
-        //Set auth header for imgur
-        setHeader: function(xhr) {
-            xhr.setRequestHeader('Authorization', 'Client-ID 312eab995957c5a');
-        },
+    function filterImgurImages(images) {
 
-        // Build a request url
-        createUrl: function(subreddit, pageCount, sort, time) {
-            var urlString = 'https://api.imgur.com/3/gallery/r/' + subreddit;
-            urlString += (sort === 'top') ? '/top/' + time : '/time';
-            return urlString + '/' + pageCount + '.json';
-        },
+        var filteredImages = images.filter((image) => {
+            // Filter out missing images
+            if (image.bandwidth === 0) return false;
+            // Filter out galleries
+            return image.link.indexOf('//imgur.com/a/') === -1;
+        });
 
-        // Filter returned images
-        imageFilter: function(images) {
+        filteredImages.forEach((image) => {
+            var isGif = image.type === 'image/gif';
+            // Set thumb
+            var thumb = image.link;
+            if (isGif && thumb.indexOf('h.gif') !== -1) {
+                thumb = thumb.replace('h.gif', 'b.gif');
+            } else {
+                thumb = thumb.replace(image.id, image.id + 'b');
+            }
+            image.thumb = thumb;
+            // Set thumbnail
+            image.display = image.link.replace(image.id, image.id + 'h')
+            image.isGif = isGif;
+        });
 
-            var filteredImages = images.filter(function(image) {
-                // Filter out missing images
-                if (image.bandwidth === 0) return false;
-                // Filter out galleries
-                return image.link.indexOf('//imgur.com/a/') === -1;
-            });
+        return filteredImages;
+    }
 
-            // Standardise display and thumbnail images
-            for (var i = 0; i < filteredImages.length; i++) {
-                var image = filteredImages[i];
-                var isGif = image.type === 'image/gif';
-                // Set thumb
-                var thumb = image.link;
-                if (isGif && thumb.indexOf('h.gif') !== -1) {
-                    thumb = thumb.replace('h.gif', 'b.gif');
+    function makeImgurRequest(subreddit, page, sort, time) {
+        return new Promise((resolve, reject) => {
+            var request = new XMLHttpRequest();
+            var url = createImgurUrl(subreddit, page, sort, time);
+            request.open('GET', url, true);
+            request.setRequestHeader('Authorization', 'Client-ID 312eab995957c5a');
+
+            request.onload = () => {
+                if (request.status >= 200 && request.status < 400) {
+                    // Success!
+                    var data = JSON.parse(request.response);
+                    var images = filterImgurImages(data.data);
+                    resolve(images);
                 } else {
-                    thumb = thumb.replace(image.id, image.id + 'b');
+                    reject(Error('Imgur request failed'));
                 }
-                image.thumb = thumb;
-                // Set thumbnail
-                image.display = image.link.replace(image.id, image.id + 'h')
-                image.isGif = isGif;
             };
 
-            return filteredImages;
-        }
+            request.onerror = () => {
+                reject(Error('Imgur request failed'));
+            };
 
-    };
+            request.send();
+        });
+    }
+
 
     var app = new Vue({
 
@@ -76,22 +87,23 @@
         },
 
         ready: function() {
-            $(this.$el).keydown((e) => {
+            // Listen to keypresses
+            this.$el.addEventListener('keydown', (event) => {
                 if (!this.currentImage) return;
                 //'D' or right arrow
-                if (e.which == 39 || e.which == 68) {
+                if (event.which == 39 || event.which == 68) {
                     this.changeImage(1);
                 }
                 //'A' or left arrow
-                if (e.which == 37 || e.which == 65) {
+                if (event.which == 37 || event.which == 65) {
                     this.changeImage(-1);
                 }
                 //escape
-                if (e.which == 27) {
+                if (event.which == 27) {
                     this.currentImage = false;
                 }
                 //Enter
-                if (e.which == 13) {
+                if (event.which == 13) {
                     if (!this.slideshow) {
                         this.startSlideshow();
                     } else {
@@ -101,15 +113,15 @@
             });
 
             // 'Infinate scroll', Auto-loads next page
-            $(window).scroll((e) => {
-                if (isScrolledIntoView('#button-load') && this.subreddit !== '') {
+            window.addEventListener('scroll', (event) => {
+                if (isScrolledIntoView('button-load') && this.subreddit !== '') {
                     this.nextPage();
                 };
             });
 
             // Autoload subreddit if in hash
             if (window.location.hash && window.location.hash !== '') {
-                var sub  = window.location.hash.substring(1);
+                var sub = window.location.hash.substring(1);
                 this.loadSubreddit(sub);
             };
         },
@@ -126,34 +138,26 @@
                 this.getImages(subreddit, this.page, this.sort, this.time)
                     .then((images) => {
                         this.images = images;
+                        this.isRequested = false;
+                    }, () => {
+                        this.isRequested = false;
                     });
             },
 
             getImages: function(subreddit, page, sort, time) {
                 this.isRequested = true;
-                return new Promise((resolve, reject) => {
-                    $.ajax({
-                        url: imgurService.createUrl(subreddit, page, sort, time),
-                        type: 'GET',
-                        dataType: 'json',
-                        beforeSend: imgurService.setHeader
-                    }).done((data) => {
-                        var images = imgurService.imageFilter(data.data);
-                        resolve(images);
-                    }).fail(() => {
-                        reject(Error('Imgur request failed'));
-                    }).always(() => {
-                        this.isRequested = false;
-                    });
-                });
+                return makeImgurRequest(subreddit, page, sort, time);
             },
 
             nextPage: function() {
+                if (this.isRequested) return false;
                 this.page = this.page + 1;
+                this.isRequested = true;
                 var imagesPromise = this.getImages(this.subreddit, this.page, this.sort, this.time);
                 return imagesPromise.then((images) => {
                     this.images = this.images.concat(images);
-                });
+                    this.isRequested = false;
+                }, () => {this.isRequested = false;});
             },
 
             // Submit handler for above subreddit loader
@@ -173,6 +177,7 @@
             hideImageDisplay: function(e) {
                 if (e.target === this.$els.imageDisplay) {
                     this.currentImage = false;
+                    this.slideshow = false;
                 }
             },
 
