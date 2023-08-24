c04c191057d81554b0b6e5e005c2c718f1a84cd4
Dan Brown <ssddanbrown@googlemail.com>
2023-08-24 17:00:40 +0100

    Updated project to vue3

--- src/components/fancy-radio.vue
+++ src/components/fancy-radio.vue
@@ -1,22 +1,25 @@
 <template>
     <div class="fancy-radio">
-        <input :id="id" :checked="value === newValue" type="radio" :value="newValue" v-on:change="updateValue">
+        <input :id="id" :checked="modelValue === newValue" type="radio" :value="newValue" v-on:change="updateValue">
         <label :for="id"><slot></slot></label>
     </div>
 </template>
 
 <script>
     export default {
-        props: ['value', 'newValue'],
-        data: function() {
+        props: {
+            modelValue: String,
+            newValue: String,
+        },
+        data() {
             return {
                 id: 'fancy-radio-' + Math.random().toString(36).substr(2, 10)
             }
         },
         methods: {
             updateValue() {
-                let formattedValue = this.newValue.trim();
-                this.$emit('input', formattedValue)
+                const formattedValue = this.newValue.trim();
+                this.$emit('update:modelValue', formattedValue)
             }
         }
     }
--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
 // The following line loads the standalone build of Vue instead of the runtime-only build,
 // so you don't have to do: import Vue from 'vue/dist/vue'
 // This is done with the browser options. For the config, see package.json
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
 
 import "./sass/screen.scss"
 
-new Vue({ // eslint-disable-line no-new
-  el: '#app',
-  render: (h) => h(App)
-})
+const app = createApp(App);
+app.mount('#app');
+
